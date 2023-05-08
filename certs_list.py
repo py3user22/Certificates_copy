90e15ff12ee5c74a652216bec281faf5c9db2d2e
Jesus G <113757515+py3user22@users.noreply.github.com>
2023-05-08 00:04:28 -0500

    Add files via upload

---
+++ certs_list.py
@@ -0,0 +1,173 @@
+#!/bin/python3
+
+
+"""Certifications list"""
+
+Google_certs = [
+    "Google IT automation",
+    "https://www.credly.com/badges/903565ad-b5fe-4c86-a585-580146c821a7/linked_in_profile",
+    "Automating real-world task w/ Python",
+    "XDHEUGZEY3P6 | https://coursera.org/share/df429fd641a4bd7ee38028a298b0205f",
+    "Troubleshooting & debugging techniques",
+    "572YVGW669FE | https://coursera.org/share/bbeebc855c9e34c48515ac95af51de68",
+    "Intro to Git & GitHub",
+    "ST6RK5SG6TV9 | https://coursera.org/share/1a169b6b9b194646a20581efe1a5466f",
+    "Using python to interact w/ the OS",
+    "GXSUJFEZMZ98 | https://coursera.org/share/1ae35ed2fcb46c1aad785ab577533b13",
+    "Crash course on python",
+    "LT2S37JZPMSA | https://coursera.org/share/d7057cfcddca2e141f142f9224cdbcd2",
+    "Set up Network & Http load balancers",
+    "A2CARJQFLEZ4 | https://coursera.org/share/1d400db08baef9d708ea517094b12767",
+    "Configuration Mgmt & the cloud",
+    "HVG8J57YYZXN | https://coursera.org/share/08136b58c345bdc05c4f53accf379ac5",
+    "Networking in Google cloud: Defining & implementing networks",
+    "RQQ67CJS3K23 | https://coursera.org/share/b4eb9cda8310f1f950789b2ee37d4427",
+]
+
+# -------------------
+
+AWS_certs = [
+    "AWS cloud quest: Cloud practitioner",
+    "https://www.credly.com/badges/54097c6d-85fa-4d4a-91f5-027c2f3b9623/linked_in_profile",
+    "AWS educate: Getting started w/ Networking",
+    "https://www.credly.com/badges/7b3edf74-e4a0-4e81-9cfe-ca2c70d9153d/linked_in_profile",
+    "AWS educate: Machine learning -DeepRacer",
+    "https://www.credly.com/badges/952e3bdf-f78e-435d-b1ed-093263a3213a/linked_in_profile",
+    "Building modern python applications on AWS",
+    "UJWEFFXJERWC | ",
+]
+
+
+Microsoft_certs = [
+    "Microsoft Azure mgmt tools & security solution",
+    "KFWCXGTC4YYR | https://coursera.org/share/15c31422fc47e147a81fd3cb32ddb04c",
+]
+
+
+Cisco_certs = [
+    "**Cyber threat mgmt",
+    "https://www.credly.com/badges/790cfc24-26d9-47f7-b854-e63925793f27/linked_in_profile",
+    "    Vulnerability Assessment & risk Mgmt",
+    "    Incident Response",
+    "**Endpoint security",
+    "https://www.credly.com/badges/ce0b2825-0653-4b52-a5a5-f3834960f396/linked_in_profile",
+    "    OS & endpoint security",
+    "    Network security basics",
+    "**Intro to Cybersecurity",
+    "https://www.credly.com/badges/7a6a3ef8-fdf0-4b69-aaf9-1ccff3650d3d/linked_in_profile",
+    "    Cybersecurity administration",
+    "    Threat Analysis",
+    "    System Safegaurds",
+    "    Network defense",
+    "    Resource specialist",
+    "**Python essentials 1",
+    "    Intro to python & programming",
+]
+
+in_progress_cisco = [
+    "Network Addressing & basic troubleshooting",
+    " | ",
+]
+
+
+Palo_Alto_certs = [
+    "Palo Alto Networks Cloud security fundamentals",
+    "SQJFL6RLNWBT | https://coursera.org/share/3b5c9197f6ca10940179bbcc13b35607",
+]
+for bb in Palo_Alto_certs:
+    print(bb)
+
+# in progress
+in_progress_palo_alto = [
+    "Palo Alto Networks Security Operations Fundamentals",
+    " | ",
+]
+
+for aa in in_progress_palo_alto:
+    print(aa)
+
+EIC_Digital = [
+    "Cybersecurity awareness & innovation",
+    "57GUGFNTZGE3 | https://coursera.org/share/65cf072c127a7d7a5d536b270f5a430f",
+]
+
+IBM_certs = [
+    "IBM Cyber Security Analyst professional",
+    "https://www.credly.com/badges/e150a6d3-bd49-4fec-b3a9-4cfc41e90dba/linked_in_profile",
+    "IT Fundamentals for Cybersecurity -Specialization",
+    "22NBQPR5DU3X | https://coursera.org/share/63e39f72b5dc1963934c16b0552b2324",
+    " >> Intro to Cybersecurity tools & cyber attacks",
+    " >>  | ",
+    " >> Cybersecurity roles, processes & OS security",
+    " >>  | ",
+    " >> Cybersecurity compliance framework & system administration",
+    " >>  | ",
+    " >> Network Security & database vulnerabilities",
+    " >>  | ",
+    "Security Analyst Fundamentals -Specialization",
+    "RL5NR4AG978U | https://coursera.org/share/0f7f2381a7599df50111265d6b9fc2cd",
+    " >> Penetration testing, Incident Response & forensics",
+    " >>  | ",
+    "    Cyber threat intelligence",
+    " | ",
+    "    Cybersecurity breach case studies",
+    " | ",
+]
+
+Coursera_AWS = [
+    "Access an EC2 instance shell from the AWS console",
+    "RP8GH2XG9BGT | https://coursera.org/share/c4d6bf7d25577af17eb839ae7e529b53"
+    "Accessing your AWS EC2 servers",
+    "QM8UVKM7KZZU | https://coursera.org/share/7565de03812768beeeaf6863c30ebc19",
+    "Create a Virtual machine using AWS EC2",
+    "YYP8L6FQXEGD | https://coursera.org/share/0498e1382eb2d11c864fe816098359b1",
+    "Create a VPC virtual private cloud using AWS"
+    "NYQE98NZ9M69 | https://coursera.org/share/61051aea97fe1f3e5aa772121058030e",
+    "AWS Multi-tier VPC Architecture",
+    "BBKQJRPRKHDU | https://coursera.org/share/41d51d5a4ed633f452a6249ea583dc6a"
+]
+
+
+Pentest_certs = [
+    "Analyze Network Traffic w/ TCPdump",
+    "YAWLPML7MDDY | https://coursera.org/share/2a5d74f8efb92d13f1ef72dc02615645 ",
+    "Wireshark for basic network security analysis"
+    " | ",
+]
+
+Networking_certs_vary = [
+    "Data forwarding across computer networks",
+    "ZN97SLL43783 | https://coursera.org/share/42de8add6730cdbaad978863d8586e3f",
+    "Config & test basic network connectivity"
+    "56MMA6QQGG6R | https://coursera.org/share/0dc9cba367cdd7abe6fae61d5970b12b",
+]
+
+Coursera_Cisco_vary = [
+    "Securing Cisco Switches w/ port security",
+    "5Z3V6ESURWZG | https://coursera.org/share/edfc78f52fa4174bc8c9ee9ba7c25bd9",
+    "Configure standard Access Lists on Cisco routers",
+    "X6ZEWHNFAYMT | https://coursera.org/share/5d94043d2c8d3bb53a9c350ea0e010e6",
+    "Configure VLANs on Cisco switches",
+    "VTUBGEV8PHX2 | https://coursera.org/share/e825165702e1f31cc9ecd5d7d8262829 ",
+    "Configure & verify EIGRP",
+    "J9EUEG3T57MY | https://coursera.org/share/c511515f30eb57931d724c948418a922",
+    "Config & verify NAT on cisco routers",
+    "TVH2BSPFNVYL | https://coursera.org/share/2466a2f8f98d9890827cbf08771afcd9",
+    "Config & verify OSPF operation",
+    "BVFZPRJJ4ABU | https://coursera.org/share/de3b901a82270c6bffe9ce9891f1e509",
+    "How to secure your network device using Cisco Packet tracer",
+    "KDW55P5JGR3Y | https://coursera.org/share/13ec638fdb44606c70ebd35f12e0198c",
+    "Intro to networks & cisco devices"
+    "CBZM3RLPZRFZ | https://coursera.org/share/7bc9fbb0a35e2795d08991c7d079eb29",
+]
+
+ISC2_certs = [
+    "Candidate badge",
+]
+
+special_vary = [
+    "How to create a Jira SCRUM project",
+    "JPAYZEK36PJ8 | https://coursera.org/share/37543e8d359b898eb44c5fbb130a4292",
+    "UoIllinois > leading teams: developing as a leader"
+    "XXH396TFMSML | ",
+]
